056c90ea97cdcb909ee179ea79239e14705be7d4
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-05-15 22:48:55 -0300

    cloudinit

--- infrastructure/cloud-init.sh
+++ infrastructure/cloud-init.sh
@@ -6,7 +6,12 @@ export NEEDRESTART_MODE=a
 sudo apt update
 sudo apt upgrade -y
 
+sudo apt install unzip -y
+
 wget https://github.com/Azure/iot-hub-device-update/releases/download/1.0.2/Tutorial_Simulator.zip
+unzip Tutorial_Simulator.zip
+cp sample-du-simulator-data.json /tmp/du-simulator-data.json
+sudo chmod 664 /tmp/du-simulator-data.json
 
 wget https://packages.microsoft.com/config/ubuntu/18.04/multiarch/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
 sudo dpkg -i packages-microsoft-prod.deb
